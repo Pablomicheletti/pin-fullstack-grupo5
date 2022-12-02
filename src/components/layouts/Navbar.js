b8fa69ce6a234395c7b0b7ee1af0c27652d0edb4
Pablo Micheletti <pabmicheletti@gmail.com>
2022-12-02 19:13:21 -0300

    se organizan componentes

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -8,7 +8,7 @@ function NavBarMain() {
   return (
     <Navbar className="navbarmain"collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Iguazú</Navbar.Brand>
+        <Navbar.Brand href="/">Iguazú</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
